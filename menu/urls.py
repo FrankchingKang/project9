6de5d8a92bfe61da3b92ba0ce76e15429a83022f
yi ching kang <frankyi7878@gmail.com>
2020-01-14 13:18:22 +0800

    add debug tool bar

--- menu/urls.py
+++ menu/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
+from django.conf import settings
 
 urlpatterns = [
     url(r'^$', views.menu_list, name='menu_list'),
@@ -8,3 +9,13 @@ urlpatterns = [
     url(r'^menu/item/(?P<pk>\d+)/$', views.item_detail, name='item_detail'),
     url(r'^menu/new/$', views.create_new_menu, name='menu_new'),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        #url(r'__debug__/', include(debug_toolbar.urls)),
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+        # For django versions before 2.0:
+        # url(r'^__debug__/', include(debug_toolbar.urls)),
+
+    ] + urlpatterns
