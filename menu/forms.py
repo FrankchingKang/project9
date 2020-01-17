d9152c9627fdb44154d19cc27bbc623710f4db12
yi ching kang <frankyi7878@gmail.com>
2020-01-17 12:13:41 +0800

    add a validators for MenuForm

--- menu/forms.py
+++ menu/forms.py
@@ -1,10 +1,24 @@
 from django import forms
 from django.forms.extras.widgets import SelectDateWidget
-
+from django.core import validators
 from .models import Menu, Item, Ingredient
+import datetime
+
+
+
+def expiration_date_check(date):
+    if date <=  datetime.today():
+        raise forms.ValidationError('expiration date need to be a future date')
+
+
 
 class MenuForm(forms.ModelForm):
 
     class Meta:
         model = Menu
+        expiration_date = forms.DateField(
+            required=False,
+            widget=SelectDateWidget(),
+            validators=[expiration_date_check]
+        )
         exclude = ('created_date',)
